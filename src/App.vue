ddb2ae674485445a8723ca764f0949794864cc54
周宝 <baobaozhou199606@gmail.com>
2017-06-09 14:28:19 +0800

    成功显示高德地图

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view></router-view>-->
+    <h3 class="title">{{ msg }}</h3>
+    <div class="amap-wrapper">
+      <el-amap :vid="'amap-vue'"></el-amap>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'vue-amap向你问好！'
+      }
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    .amap-wrapper {
+      width: 500px;
+      height: 500px;
+    }
+
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,27 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+import AMap from 'vue-amap'
+
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  key: 'your amap key',
+  plugin: [
+    'AMap.Autocomplete',
+    'AMap.PlaceSearch',
+    'AMap.Scale',
+    'AMap.OverView',
+    'AMap.ToolBar',
+    'AMap.MapType',
+    'AMap.PolyEditor',
+    'AMap.CircleEditor'
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
