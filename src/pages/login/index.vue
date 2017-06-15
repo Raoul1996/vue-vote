3d6d97bdee9b99d019cab4ca6d8e0877032cb7c8
周宝 <baobaozhou199606@gmail.com>
2017-06-15 20:23:43 +0800

    调整项目结构，增加pages目录

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -86,4 +86,9 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .login {
+    width 50%
+    margin 0 auto
+  }
+</style>
