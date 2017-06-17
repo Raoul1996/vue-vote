54b846e8f54f18093a806b3d4fbae90e05ddcf6d
周宝 <baobaozhou199606@gmail.com>
2017-06-17 17:55:50 +0800

    使用mongoDB实现简单的登陆注册功能，

--- server/database/models.js
+++ server/database/models.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 let UserSchma = new Schema({
   mobile: String,
-  password: String
-  // token: String
+  password: String,
+  token: String
 })
 module.exports = mongoose.model('User', UserSchma)
